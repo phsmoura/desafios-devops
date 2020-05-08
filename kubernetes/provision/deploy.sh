265851a6e2b140d45fc592b75c146535a5200550
Pedro Moura <moura.pedro123@gmail.com>
2020-05-08 05:51:17 -0300

    improved script with function and variable

--- kubernetes/provision/deploy.sh
+++ kubernetes/provision/deploy.sh
@@ -1,50 +1,43 @@
 #!/bin/bash
 
-function print_help {
-	echo "Usage: ./deploy.sh [--all|--namespace|--service|--deploy] [--wait]
+DIR_MANIFESTS="/home/vagrant/kubernetes/manifests"
 
-Mandatory arguments:
+function print_help {
+	echo "Usage: ./deploy.sh [--all|--namespace|--service|--deployment]
+
+Option arguments:
 --all		apply namespace, service and deployment
 --namespaces	apply only namespace
 --service	apply only service
---deploy	apply only deployment
-
-Not mandatory options:
---wait	wait 20 seconds before applying changes
+--deployment	apply only deployment
 --help	show this option
 "
 }
 
-if [ $# -ne 2 ]
+if [ $# -gt 1 ]
 then
 	print_help
 	exit
 fi
 
-# wait for minion to get ready on cluster
-if [ $2 == "--wait" ]
-then
-	sleep 20
-fi
-
 case $1 in
   --all)
-  kubectl apply -f /home/vagrant/kubernetes/manifests/app-namespace
-  kubectl apply -f /home/vagrant/kubernetes/manifests/app-service
-  kubectl apply -f /home/vagrant/kubernetes/manifests/app-deployment
+  kubectl apply -f $DIR_MANIFESTS/app-namespace.yml
+  kubectl apply -f $DIR_MANIFESTS/app-service.yml
+  kubectl apply -f $DIR_MANIFESTS/app-deployment.yml
   # kubectl apply -f /home/vagrant/kubernetes/manifests/app-ingress
   ;;
 
 	--namespace)
-	kubectl apply -f /home/vagrant/kubernetes/manifests/app-namespace
+	kubectl apply -f $DIR_MANIFESTS/app-namespace.yml
 	;;
 
 	--service)
-	kubectl apply -f /home/vagrant/kubernetes/manifests/app-service
+	kubectl apply -f $DIR_MANIFESTS/app-service.yml
 	;;
 
-	--deploy)
-	kubectl apply -f /home/vagrant/kubernetes/manifests/app-deployment
+	--deployment)
+	kubectl apply -f $DIR_MANIFESTS/app-deployment.yml
 	;;
 
 	--help)
@@ -52,8 +45,7 @@ case $1 in
 	;;
 
   *)
-	echo "
-	Invalid option!"
+	echo "Invalid option!"
 	print_help
   ;;
 esac
