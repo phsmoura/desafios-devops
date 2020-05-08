f949e4a321db9b8b8e8f3e33e751070635c9eef5
Pedro Moura <moura.pedro123@gmail.com>
2020-05-08 05:53:25 -0300

    add registry container on localhost for testing purpose

--- kubernetes/provision/minion.sh
+++ kubernetes/provision/minion.sh
@@ -4,6 +4,10 @@
 echo "KUBELET_EXTRA_ARGS='--node-ip=172.100.100.$1'" > /etc/default/kubelet
 $(ssh -o stricthostkeychecking=no 172.100.100.10 kubeadm token create --print-join-command)
 
-# building app image
+# deploying registry server
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
+
+# building app image and push to registry
 docker image build --tag=app:1.0 /home/vagrant/kubernetes/
-docker image tag app:1.0 app
+docker tag app:1.0 localhost:5000/app
+docker push localhost:5000/app
